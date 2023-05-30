5f955a7cbe6c84230ef3ea7a5eefff0b0eae56fb
Penelope <freyalabram@gmail.com>
2023-05-30 20:13:50 +0200

    check changes have uploaded after crash

---
+++ constants.js
@@ -0,0 +1,22 @@
+export const DOCUMENTATION = [
+  {
+    title: "The best of React",
+    url: "",
+    favorite: true
+  },
+  {
+    title: "React docs",
+    url: "",
+    favorite: false
+  },
+  {
+    title: "JS documentation",
+    url: "",
+    favorite: true
+  },
+  {
+    title: "Ejemplos de CSS",
+    url: "",
+    favorite: false
+  }
+]
